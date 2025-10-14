9c19a6384d27346a4fd98ce9639931544b76ce54
maciej <you@example.com>
2025-10-14 15:14:14 +0200

    Normalize all line endings to LF

--- src/db/supabase.client.ts
+++ src/db/supabase.client.ts
@@ -6,4 +6,3 @@ const supabaseUrl = import.meta.env.SUPABASE_URL;
 const supabaseAnonKey = import.meta.env.SUPABASE_KEY;
 
 export const supabaseClient = createClient<Database>(supabaseUrl, supabaseAnonKey);
-// comment
